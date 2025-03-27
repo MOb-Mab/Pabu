a471cd247b66daca5f0c5af39c6ba82acc6a15a3
MOb-Mab <151912540+MOb-Mab@users.noreply.github.com>
2025-03-27 11:56:47 +0700

    Update

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Homescreen from "./src/screens/Homescreen";
+import ListCard from "./src/component/ParkingSlot";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import CarparkingScreen from "./src/screens/CarparkingScreen";
+import ReservationScreen from "./src/screens/ReservationScreen";
+import PaymentScreen from "./src/screens/PaymentScreen";
+import MyParkingScreen from "./src/screens/MyparkingScreen";
+const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"> 
+        <Stack.Screen 
+        name="Home" 
+        component={Homescreen}
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+        name="Register" 
+        component={RegisterScreen}
+        options={{ headerShown: false }}
+         />
+         <Stack.Screen 
+        name="Login" 
+        component={LoginScreen}
+        options={{ headerShown: false }}
+        />
+         <Stack.Screen 
+        name="Parking" 
+        component={CarparkingScreen}
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+        name="Reservation" 
+        component={ReservationScreen}
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+        name="Payment" 
+        component={PaymentScreen}
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+        name="Myparking" 
+        component={MyParkingScreen}
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+        name="ListCard" 
+        component={ListCard}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
